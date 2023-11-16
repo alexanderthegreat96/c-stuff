ce0756d55ee2cde59b86ac33ae21e53883ec66f3
Alexandru <alexandrulupaescu@gmail.com>
2023-11-16 19:16:06 +0200

    adding a test program

---
+++ first_program/first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// should return 0
+// if it returns 1
+// we got an issue
+
+int main() {
+    printf("Hello, this is the first program written in C!\n");
+
+    // keep the window open
+    printf("Press ENTER to exit...");
+    getchar();
+    return 0;
+}
+
+// to compile the program
+
+// gcc program.c -o /your-parth-goes-here
